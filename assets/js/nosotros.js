705387f92004094eda725d0aa65c52c0a9b48827
Gonzalo Fuentes <gonzalo.f.8c@gmail.com>
2022-05-21 20:25:55 -0400

    Carrusel

---
+++ assets/js/nosotros.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+$('#Mostrar').on('click',function(){
+    $('#parrafo-accion02').show(1000);
+});
+$('#Ocultar').on('click',function(){
+    $('#parrafo-accion02').hide(1000);
+});
+})
