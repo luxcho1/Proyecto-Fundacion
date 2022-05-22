70d8c0e9215c795e8c8cbcae151e93b232d1abca
Gonzalo Fuentes <gonzalo.f.8c@gmail.com>
2022-05-22 17:18:37 -0400

    Jquery

--- assets/js/nosotros.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-$('#Mostrar').on('click',function(){
-    $('#parrafo-accion02').show(1000);
-});
-$('#Ocultar').on('click',function(){
-    $('#parrafo-accion02').hide(1000);
-});
-})
