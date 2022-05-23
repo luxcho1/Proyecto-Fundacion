6caff0f03f38de0404873bfba0961a6987b6d5b1
Gonzalo Fuentes <gonzalo.f.8c@gmail.com>
2022-05-23 19:29:10 -0400

    Doble Hoover, verificada en validator, sin errores

--- assets/js/nosotros.js
+++ assets/js/nosotros.js
@@ -39,24 +39,4 @@ $(document).ready(function() {
         $('.parrafo-accion04').hide();
         $('.Ocultar4').hide();
     });
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-                nav:true
-            },
-            600:{
-                items:3,
-                nav:false
-            },
-            1000:{
-                items:5,
-                nav:true,
-                loop:false
-            }
-        }
-    })
     })
