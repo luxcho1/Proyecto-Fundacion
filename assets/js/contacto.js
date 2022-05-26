d8e15165b2694680d83d32fd3bad087d6b7b4ef9
Gabriel Toledo <gabrielons2010@gmail.com>
2022-05-26 00:53:53 -0400

    modo escuro en todas las paginas con el reloj

--- assets/js/contacto.js
+++ assets/js/contacto.js
@@ -268,6 +268,48 @@ const cambiarModo = () => {
 
 }
 
+// Funcion Relog
+function clock(){
+  var today = new Date();
+
+  var hours = today.getHours();
+  var minutes = today.getMinutes();
+  var seconds = today.getSeconds();
+
+  if (hours < 10){
+    hours = "0" + hours;
+  }
+
+  if(minutes < 10){
+  minutes = "0" + minutes;
+}
+
+if(seconds < 10){
+  seconds = "0" + seconds;
+}
+
+  document.querySelector(".hours").innerHTML = hours;
+  document.querySelector(".minutes").innerHTML = minutes;
+  document.querySelector(".seconds").innerHTML = seconds;
+}
+
+var updateClock = setInterval(clock,1000);
+
+
+var today = new Date();
+const dayNumber = today.getDate();
+const year = today.getFullYear();
+const dayName = today.toLocaleDateString("default",{weekday:"long"});
+const monthName = today.toLocaleDateString("default",{month:"short"});
+
+document.querySelector(".month-name").innerHTML = monthName
+document.querySelector(".day-name").innerHTML = dayName
+document.querySelector(".day-number").innerHTML = dayNumber
+document.querySelector(".year").innerHTML = year
+
+
+
+
 /*********************
     JS FOOTER
 **********************/
